0e24d6493d1b198d60a61dd326546bac4ea85877
IceVladimir <74771948+IceVladimir@users.noreply.github.com>
2020-11-20 15:49:36 +0400

    Update first.py
    i added a new line

--- first.py
+++ first.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('And now im cool')
-
+    print('Okay its more coolest than i think')
 
 if __name__ == '__main__':
     main()
